9d03b2d5828546b4f0681d23b754275af3813976
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2024-11-11 10:24:00 +0000

    Add change to branch

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,5 @@
 
-const apiURL = `https://opentdb.com/api.php?amount=10&category=9&difficulty=medium`;
+const apiURL = `https://opentdb.com/api.php?amount=10&category=9&type=boolean`;
     fetch(apiURL)                        // retrieve all data from this url
     .then(function (response) {          // this gets called when the fetch function returns its data
         return response.json();          // convert the relevant part of the page to a JS object
