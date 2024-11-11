4f99d1c0a3939c89f51fe9523b5757cc75d1215e
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2024-11-11 09:25:36 +0000

    Add basic file structure and html format and retrieve API

---
+++ assets/scripts/script.js
@@ -0,0 +1,8 @@
+
+const apiURL = `https://opentdb.com/api.php?amount=10&category=9&difficulty=medium`;
+    fetch(apiURL)                        // retrieve all data from this url
+    .then(function (response) {          // this gets called when the fetch function returns its data
+        return response.json();          // convert the relevant part of the page to a JS object
+    }).then(function (object) {          // this gets called when the conversion is complete
+        console.log(object);
+    });
